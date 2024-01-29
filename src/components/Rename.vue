5da0a5f743142df92c5f71c99eefcc66ee1e07ce
alfaruqii <bumbblechover@gmail.com>
2024-01-29 23:15:03 +0700

    fix: bug in Rename component (cannot update event with space character)

--- src/components/Rename.vue
+++ src/components/Rename.vue
@@ -1,11 +1,11 @@
 <template>
-  <Disclosure>
-    <DisclosureButton class="flex gap-2 w-full items-center text-left font-medium outline-none px-3 py-2 ">
+  <Disclosure as="div">
+    <DisclosureButton as="div" class="flex gap-2 w-full items-center text-left font-medium outline-none px-3 py-2 ">
       <v-icon name="md-modeeditoutline" />
       <p class="">Rename</p>
     </DisclosureButton>
-    <DisclosurePanel class="px-4 pt-3 pb-3 border-t text-sm">
-      <input type="text" class="border-b border-[#FE6367] outline-none mb-1" v-model="newName.habit"
+    <DisclosurePanel as="div" class="px-4 pt-3 pb-3 border-t text-sm">
+      <input type="text" class="border-b border-[#FE6367] outline-none mb-1" v-model="newName.habit" v-on:keydown.stop
         placeholder="new name" @keydown.enter="changeName" />
       <div class="flex justify-around gap-2.5 pt-3">
         <button class="rounded bg-gray-200 px-3 py-1.5 hover:bg-gray-300" @click.prevent="changeName"
