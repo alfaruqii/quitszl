8a7690019025821e17b9f694aaf524528a6f4182
alfaruqii <bumbblechover@gmail.com>
2024-08-03 19:47:45 +0700

    feat: remove Quote

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="px-4 sm:w-3/6 sm:mx-auto pb-8 min-h-screen">
-    <Quote />
     <Habit />
   </div>
 </template>
 
 <script setup>
-import Quote from '../components/Quote.vue';
-import Habit from '../components/Habit.vue';
+import Habit from "../components/Habit.vue";
 </script>
